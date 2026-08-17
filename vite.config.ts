5408d44067117a5e46860484df984edb704779fd
TAKAHASHI Shuuji <id@shuuji3.xyz>
2026-08-17 16:15:15 +0900

    feat: implement app

--- vite.config.ts
+++ vite.config.ts
@@ -8,15 +8,12 @@ export default defineConfig({
     "*": "vp check --fix",
   },
   fmt: {
-    ignorePatterns: ["public/maplibre-gl-*.mjs", "public/photos.json"],
+    ignorePatterns: ["public/photos.json"],
   },
   lint: {
-    ignorePatterns: ["public/maplibre-gl-*.mjs", "public/photos.json"],
+    ignorePatterns: ["public/photos.json"],
     jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
     rules: { "vite-plus/prefer-vite-plus-imports": "error" },
     options: { typeAware: true, typeCheck: true },
-  },
-  server: {
-    allowedHosts: ["tmp-takahashi.shuuji3.xyz"],
   },
 });
